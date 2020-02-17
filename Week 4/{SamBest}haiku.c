8a1f88411b68f492c5a39af17b6d2b53439f0032
sbest33 <60371042+sbest33@users.noreply.github.com>
2020-02-17 17:14:45 -0500

    Add files via upload

---
+++ Week 4/{SamBest}haiku.c
@@ -0,0 +1,31 @@
+#include <stdio.h> /* telling it to include the standard C library */
+int main(void)
+{
+  printf("In fat armchairs sat\a\n");
+  printf("indolence and impatience\a\n");
+  printf("plotting my downfall.\a\n");
+  printf("A wicked cage flew\a\n");
+  printf("across the long horizon\a\n");
+  printf("searching for a bird.\a\n");
+  printf("I burned with love in\a\n");
+  printf("empty rooms, I sternly turned\a\n");
+  printf("knives within myself.\a\n");
+  printf("\"Behold the bright gate,\"\a\n"); /* I had to check on Stack
+  Overflow how to place double quotes within a String */
+  printf("the keeper said. \"I am now\a\n");
+  printf("goin to shut it.\"\a\n");
+  printf("Hardly was the road\a\n");
+  printf("swept clean when ah! there appeared\a\n");
+  printf("new piles of dry leaves.\a\n");
+  printf("But nothing could kill\a\n");
+  printf("a faith like a guillotine,\a\n");
+  printf("as heavy, as light.\a\n");
+  printf("Happinness? Finding\a\n");
+  printf("your indestructible core;\a\n"); /* I had to check on Stack Overflow
+  whether semicolons had any significance within a String but they don't */
+  printf("leaving it alone.\a\n");
+  printf("Into the heavens\a\n");
+  printf("flew a breathless legion of\a\n");
+  printf("impossible crows.\a\n");
+  return 0; /* just to end the program */
+}
